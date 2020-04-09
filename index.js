60b3994a97726675f2ce5bf89d245144ef14af09
Mansoor Omrani <mansoor.omrani@gmail.com>
2020-04-09 09:26:57 +0430

    Used locustjs-extensions-options to populate options and checking if an extension should be applied

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { isObject } from 'locust-base'
+import { configureOptions, shouldExend } from 'locustjs-extensions-options'
 
 const isSubClassOf = (child, parent) => child.prototype instanceof parent || child === parent;
+
 //source: https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
 const deepAssign = function (target, ...sources) {
 	if (!sources.length) return target;
@@ -24,17 +26,15 @@ const deepAssign = function (target, ...sources) {
 }
 
 function configureObjectExtensions(options) {
-	const _options = Object.assign({
-		replaceExisting: true
-	}, options);
+	const _options = configureOptions(options)
 
-	if (typeof Object.prototype.isSubClassOf == 'undefined' || _options.replaceExisting) {
+	if (typeof Object.prototype.isSubClassOf == 'undefined' || shouldExend('isSubClassOf', _options)) {
 		Object.prototype.isSubClassOf = function (parent) {
 			return isSubClassOf(this, parent);
 		}
 	}
 	
-	if (typeof Object.prototype.toJson == 'undefined' || _options.replaceExisting) {
+	if (typeof Object.prototype.toJson == 'undefined' || shouldExend('toJson', _options)) {
 		Object.prototype.toJson = function (replacer = null, space = null) {
 			return JSON.stringify(this, relacer, space)
 		}
@@ -43,7 +43,7 @@ function configureObjectExtensions(options) {
 	Object.deepAssign = deepAssign;
 }
 
-export default configureObjectExtensions()
+export default configureObjectExtensions
 
 export {
 	isSubClassOf,
